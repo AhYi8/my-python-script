d5c8e91b66a005b593f0801709d331a3e7235655
阿一 <1457571009@qq.com>
2024-10-15 14:05:04 +0800

    202410151404

--- Vip91ChuangYeUtils.py
+++ Vip91ChuangYeUtils.py
@@ -1,20 +1,16 @@
-import os, logging, requests, redis, pytz, re, random, collections.abc
+import os, logging, requests, redis, pytz, re, random, collections.abc, time, schedule
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from dateutil import parser
 from retrying import retry
 from bs4 import BeautifulSoup
-from openpyxl import Workbook, load_workbook
+from openpyxl import load_workbook
 from wordpress_xmlrpc import Client, WordPressPost, ServerConnectionError
-from wordpress_xmlrpc.methods.posts import GetPosts, NewPost, EditPost, DeletePost, GetPost
-from wordpress_xmlrpc.methods.users import GetUserInfo
-from wordpress_xmlrpc.methods import media, posts
+from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
 from wordpress_xmlrpc.methods import taxonomies
-from wordpress_xmlrpc import WordPressTerm
-from wordpress_xmlrpc.compat import xmlrpc_client
 from openpyxl import Workbook
-import pandas as pd
 from typing import List
+from urllib.parse import urlparse
 
 collections.Iterable = collections.abc.Iterable
 
@@ -33,10 +29,16 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 # 将格式器应用于文件处理器
 file_handler.setFormatter(formatter)
 
+# 创建一个 StreamHandler 用于输出到控制台
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
 # 获取根日志记录器并配置
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)  # 设置日志级别
-logger.addHandler(file_handler)  # 添加处理器
+logger.addHandler(file_handler)  # 添加文件处理器
+logger.addHandler(console_handler)  # 添加控制台处理器
+
 
 class Article:
     title: str
@@ -49,13 +51,13 @@ class Article:
     def __init__(self):
         super()
 
+
 class WordpressUtils:
     wp = Client(
         r'http://res.21zys.com/xmlrpc_Mh359687...php',
         '21zys',
         'Mh359687..'
     )
-
 
     @staticmethod
     def get_all_tag_name():
@@ -142,7 +144,7 @@ class DateUtils:
     @staticmethod
     def datetime_to_str(date: datetime, fmt: str = "%Y-%m-%d %H:%M:%S"):
         return date.strftime(fmt)
-        
+
     @staticmethod
     def iso_str_to_datetime(iso_str: str) -> datetime:
         """将带时区的 ISO 8601 格式时间字符串转换为 datetime 对象"""
@@ -224,6 +226,7 @@ class DateUtils:
         if datetime_obj.tzinfo is not None:
             return datetime_obj.replace(tzinfo=None)
         return datetime_obj
+
 
 class RedisUtils:
     # Redis 连接属性，用户提供
@@ -371,10 +374,27 @@ class RedisUtils:
         cls._initialize_client()
         return cls._redis_client.zcard(key)
 
+
+class FileUtils:
+    @staticmethod
+    def read_file(path: str, is_strip: bool = False, mode: str = 'r', encoding: str = 'u8'):
+        if path is not None and len(path.strip()) != 0 and os.path.exists(path):
+            try:
+                with open(path, mode=mode, encoding=encoding) as rf:
+                    data_list = rf.readlines()
+                if is_strip:
+                    data_list = [data.strip() for data in data_list]
+                return data_list
+            except BaseException as e:
+                print(f"FileUtils.read_file(): {e}")
+                return None
+        else:
+            return None
+
 # 配置日志
 class Vip91ChuangYeUtils:
-    cookie: str = r'wordpress_test_cookie=WP%20Cookie%20check; PHPSESSID=1622mfuk602mh0ffcuk4scar64; Hm_lvt_ef6451eccfe96478ed5790d42dc4f1be=1728728301,1728900656; HMACCOUNT=573DE47D60EEFA01; wordpress_logged_in_af89e27944e71791976296dfc34e93be=21zys%7C1730110378%7CTpxUQij1wMMXT2kUkQteoGIuh68LK5ZfIeOBI9KKSgJ%7C962c1fcb505d29e890faf72a5840770c0f2c4a42c83f64cdf56e1cce2b044b81; Hm_lpvt_ef6451eccfe96478ed5790d42dc4f1be=1728900784'
     tag_names = WordpressUtils.get_all_tag_name()
+
     @staticmethod
     def convert_cookie_to_dict(cookie_str):
         """
@@ -396,7 +416,7 @@ class Vip91ChuangYeUtils:
     @staticmethod
     def fetch_url(url: str, headers: dict = None, retries=1):
         """发送HTTP请求，默认重试1次"""
-        logging.info(f"Fetching URL: {url}")
+        # logging.info(f"Fetching URL: {url}")
         attempt = 0
         while attempt <= retries:
             try:
@@ -411,29 +431,23 @@ class Vip91ChuangYeUtils:
                 if attempt == retries:
                     return None  # 超过最大重试次数，返回None
                 attempt += 1
-            
-    @staticmethod
-    def publish_vip_91_chuangye_article():
-        article_metas = Vip91ChuangYeUtils.get_vip_practical_project_meta()
-        for article_meta in article_metas:
-            link, title, cover, category, content, source_url, source_pwd, publish_date = article_meta
-            article: Article = Vip91ChuangYeUtils.article_meta_article(title, content, category, source_url, source_pwd, publish_date)
-            logging.info(f"正在采集发布：{link}-->{title}")
-            post_id = WordpressUtils.post_article(article)
-            if post_id:
-                RedisUtils.add_set(RedisUtils.vip_91_article_links, link)
 
     @staticmethod
-    def get_vip_practical_project_meta(base_url: str = "https://vip.91chuangye.cn/scxm",
-                                       is_append_xlsx: bool = True,
-                                       xlsx_file_path: str = os.path.join(os.getcwd(), "file", "vip_91chuangye_com.xlsx")):
+    def publish_vip_91_chuangye_article(base_url,
+                                        cookies,
+                                        is_append_xlsx: bool = True,
+                                        xlsx_file_path: str = os.path.join(os.getcwd(), "file", "vip_91chuangye_com.xlsx")):
+        if base_url.endswith("/"):
+            base_url = base_url[:-1]
+
         page = 1
         published_article_links = RedisUtils.get_set(RedisUtils.vip_91_article_links)
         flag = True
-        last_week_date = DateUtils.date_to_datetime(DateUtils.add_days(DateUtils.get_current_date(), -7))
-        article_metas = []
-        results = []
+        stop_date = DateUtils.date_to_datetime(DateUtils.add_days(DateUtils.get_current_date(), -3))
+
         while flag:
+            article_metas = []
+            results = []
             if page != 1:
                 url = f"{base_url}/page/{page}"
             else:
@@ -444,14 +458,14 @@ class Vip91ChuangYeUtils:
                 break  # 如果请求失败，退出循环
 
             soup = BeautifulSoup(html_content, 'html.parser')
-            
+
             post_wrapper = soup.find('div', class_='row posts-wrapper scroll')
-            
+
             item_list = post_wrapper.find_all('div', recursive=False) if post_wrapper else []
 
             for item in item_list:
                 try:
-                
+
                     # 提取title和link
                     entry_wrapper = item.find('div', class_='entry-wrapper')
                     title_tag = entry_wrapper.find('h2', class_='entry-title').find('a')
@@ -461,7 +475,7 @@ class Vip91ChuangYeUtils:
                     if published_article_links and link in published_article_links:
                         continue
 
-                    source_url, source_pwd, article_content = Vip91ChuangYeUtils.get_vip_practical_project_content(link, title)
+                    source_url, source_pwd, article_content = Vip91ChuangYeUtils.get_vip_practical_project_content(link, title, cookies)
 
                     # 提取cover
                     cover_tag = item.find('div', class_='entry-media').find('img')
@@ -470,52 +484,99 @@ class Vip91ChuangYeUtils:
                     # 提取category
                     category_tags = entry_wrapper.find('span', class_='meta-category-dot').find_all('a')
                     category = ",".join(a.text.strip() for a in category_tags)
-                    
+
                     publish_date_tags = entry_wrapper.find('span', class_='meta-date').find('time')
-                    publish_date = DateUtils.to_naive(DateUtils.iso_str_to_datetime(publish_date_tags['datetime'])) if publish_date_tags else DateUtils.get_current_datetime()
+                    publish_date = DateUtils.to_naive(DateUtils.iso_str_to_datetime(
+                        publish_date_tags['datetime'])) if publish_date_tags else DateUtils.get_current_datetime()
 
-                    if DateUtils.is_before(publish_date, last_week_date):
-                        flag = False
-                        break
+                    # if DateUtils.is_before(publish_date, stop_date):
+                    #     flag = False
+                    #     break
 
-                    logging.info(f"Link: {link}, Title: {title}, Cover: {cover}, Category: {category}, Publish_date: {publish_date}")
-                    results.append([link, title, cover, category, article_content, source_url, source_pwd, DateUtils.datetime_to_str(publish_date)])
+                    # logging.info(f"Link: {link}, Title: {title}, Cover: {cover}, Category: {category}, Publish_date: {publish_date}")
+                    results.append([link, title, cover, category, article_content, source_url, source_pwd,DateUtils.datetime_to_str(publish_date)])
                     article_metas.append([link, title, cover, category, article_content, source_url, source_pwd, publish_date])
                 except Exception as e:
                     logging.error(f"Error processing article: {e}")
                     continue
-
             # 下一页
             page += 1
-        if is_append_xlsx:
-            # 将结果追加到xlsx文件中
-            Vip91ChuangYeUtils.append_to_xlsx(xlsx_file_path, results)
-        return article_metas
+            if is_append_xlsx:
+                # 将结果追加到xlsx文件中
+                Vip91ChuangYeUtils.append_to_xlsx(xlsx_file_path, results)
 
-
+            if article_metas:
+                for article_meta in article_metas:
+                    link, title, cover, category, content, source_url, source_pwd, publish_date = article_meta
+                    article: Article = Vip91ChuangYeUtils.article_meta_article(title, content, category, source_url, source_pwd, publish_date)
+                    logging.info(f"正在采集发布：{link}-->{title}")
+                    post_id = WordpressUtils.post_article(article)
+                    if post_id:
+                        RedisUtils.add_set(RedisUtils.vip_91_article_links, link)
 
     @staticmethod
-    def get_vip_practical_project_content(url: str, title: str):
+    def get_vip_practical_project_content(url: str, title: str, cookies: str):
         # 自定义请求头
-        headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Accept-Encoding': 'gzip, deflate, br, zstd',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Cache-Control': 'max-age=0',
-            'Connection': 'keep-alive',
-            'Cookie': Vip91ChuangYeUtils.cookie,
-            'Host': 'vip.91chuangye.cn',
-            'Referer': 'https://vip.91chuangye.cn/',
-            'Sec-Fetch-Dest': 'document',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-User': '?1',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
-            'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"'
-        }
+        if 'vip.91chuangye' in url:
+            headers = {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Cache-Control': 'max-age=0',
+                'Connection': 'keep-alive',
+                'Cookie': cookies,
+                'Host': 'vip.91chuangye.cn',
+                'Referer': 'https://vip.91chuangye.cn/',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'same-origin',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+                'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"'
+            }
+        elif 'bbs.abab9' in url:
+            # 使用urlparse解析URL
+            parsed_url = urlparse(url)
+            path = parsed_url.path  # 获取URL的路径
+
+            # 定义请求头
+            headers = {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Cache-Control': 'max-age=0',
+                'Cookie': cookies,
+                'Priority': 'u=0, i',
+                'Sec-Ch-Ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+                'Sec-Ch-Ua-Mobile': '?0',
+                'Sec-Ch-Ua-Platform': '"Windows"',
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-Site': 'none',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+            }
+        else:
+            headers = {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'Accept-Encoding': 'gzip, deflate, br, zstd',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Cache-Control': 'max-age=0',
+                'Cookie': cookies,  # 假设 `cookies` 是一样的
+                'Sec-Fetch-Dest': 'document',
+                'Sec-Fetch-Mode': 'navigate',
+                'Sec-Fetch-User': '?1',
+                'Upgrade-Insecure-Requests': '1',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+                'Sec-Ch-Ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+                'Sec-Ch-Ua-Mobile': '?0',
+                'Sec-Ch-Ua-Platform': '"Windows"'
+            }
+
 
         html_content = Vip91ChuangYeUtils.fetch_url(url, headers)
         soup = BeautifulSoup(html_content, 'html.parser')
@@ -549,7 +610,6 @@ class Vip91ChuangYeUtils:
         pwd = pwd_match.group(1) if pwd_match else ""
         return url, pwd, article_content
 
-
     @staticmethod
     def article_meta_article(title, content, category, source_url, source_pwd, publish_date):
         article = Article()
@@ -557,7 +617,8 @@ class Vip91ChuangYeUtils:
         article.content = content.replace(r"<img", r"<img class='aligncenter'")
         article.date = publish_date
         article.post_status = "publish"
-        tags = {tag_name for tag_name in Vip91ChuangYeUtils.tag_names if tag_name in article.content or tag_name in title}
+        tags = {tag_name for tag_name in Vip91ChuangYeUtils.tag_names if
+                tag_name in article.content or tag_name in title}
         terms_names = {'post_tag': list(tags), 'category': category.split(",")}
         article.terms_names = terms_names
 
@@ -587,7 +648,6 @@ class Vip91ChuangYeUtils:
         article.custom_fields = custom_fields
         return article
 
-
     @staticmethod
     def append_to_xlsx(file_path, data):
         """将提取到的结果追加到现有的xlsx文件中，如果不存在则创建新文件"""
@@ -614,7 +674,24 @@ def enable_proxy():
     os.environ['https_proxy'] = 'http://localhost:10809'
     print("全局代理已开启")
 
-if __name__ == "__main__":
+def schedule_publish_task():
     enable_proxy()
+    item: str
+    url_cookies = [(item.split(": ")[0].strip(), item.split(": ")[1].strip()) for item in FileUtils.read_file(os.path.join(os.getcwd(), "file", "cookies.txt"), is_strip=True) if not item.startswith("#")]
+    for url, cookies in url_cookies:
+        Vip91ChuangYeUtils.publish_vip_91_chuangye_article(url, cookies)
+
+
+
+if __name__ == "__main__":
+
     # 调用静态方法执行操作
-    Vip91ChuangYeUtils.publish_vip_91_chuangye_article()
+    schedule_publish_task()
+
+    # 定义每天定时执行的任务
+    schedule.every(1).day.at("08:00").do(schedule_publish_task)  # 设置为每天08:00执行
+    while True:
+        detect_interval = 60 * 60
+        schedule.run_pending()  # 检查是否有任务需要执行
+        logging.info(f"等待 {detect_interval} 秒，检测数据。")
+        time.sleep(detect_interval)  # 暂停60秒
--- test.py
+++ test.py
@@ -1,55 +1,48 @@
-import requests, re, os, time, string, redis
-from retrying import retry
+import requests
 from bs4 import BeautifulSoup
-from openpyxl import Workbook, load_workbook
-from openpyxl.worksheet.datavalidation import DataValidation
-from PIL import Image
-from io import BytesIO
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from typing import List
+from urllib.parse import urlparse
 
-class FileUtils:
+# 定义URL
+url = 'https://bbs.abab9.cn/2024/10/14/267752/'
 
-    @staticmethod
-    def read_file(path: str, is_strip: bool = False, mode: str = 'r', encoding: str = 'u8'):
-        if path is not None and len(path.strip()) != 0 and os.path.exists(path):
-            try:
-                with open(path, mode=mode, encoding=encoding) as rf:
-                    data_list = rf.readlines()
-                if is_strip:
-                    data_list = [data.strip() for data in data_list]
-                return data_list
-            except BaseException as e:
-                print(f"FileUtils.read_file(): {e}")
-                return None
-        else:
-            return None
+# 使用urlparse解析URL
+parsed_url = urlparse(url)
+path = parsed_url.path  # 获取URL的路径
 
-    @staticmethod
-    def write_file(path: str, data_list, mode: str = 'w', encoding: str = 'u8'):
-        if path is not None and len(path.strip()) != 0:
-            parent_path = os.path.split(path)[0]
-            if not os.path.exists(parent_path):
-                os.makedirs(parent_path)
-            try:
-                write_list = []
-                for data in data_list:
-                    if not data.endswith("\n"):
-                        data = data + '\n'
-                    write_list.append(data)
+# 定义请求头
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-encoding': 'gzip, deflate, br, zstd',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'cache-control': 'max-age=0',
+    'cookie': 'wordpress_logged_in_b8a556d26254189b46ca27506ee3d0e7=21zys%7C1730125460%7CcdugIbXSf65XM4wFmXWa1gvGSDjyS7c1DB5x5BpZSIq%7Cd3a41d1648a283a1e498c52bfd1d606df13d18acd4ac1297a05bb8e0c515df91',
+    'priority': 'u=0, i',
+    'sec-ch-ua': '"Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+}
 
-                with open(path, mode=mode, encoding=encoding) as wf:
-                    wf.writelines(write_list)
-            except BaseException as e:
-                print(f"FileUtils.read_file(): {e}")
+# 发送GET请求
+response = requests.get(url, headers=headers)
 
-    @staticmethod
-    def remove_duplicate_logs(log_path: str = os.path.join(os.getcwd(), 'file', 'logs.txt')):
-        logs: set = {log for log in FileUtils.read_file(log_path, is_strip=True) if '跳过采集' not in log and '文章不存在' not in log}
-        FileUtils.write_file(log_path, logs)
-        
+# 检查请求是否成功
+if response.status_code == 200:
+    print("请求成功，解析HTML内容...")
+    # 解析HTML
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-links = []
-for i in range(5, 92618):
-    links.append(f"https://t.me/kuakeyun/{i}")
-FileUtils.write_file(os.path.join(os.getcwd(), 'file', 'test.txt'), links)
+    # 打印HTML的标题
+    title = soup.title.string if soup.title else "无标题"
+    print("网页标题:", title)
+
+    # 示例：打印网页中的所有链接
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+else:
+    print(f"请求失败，状态码: {response.status_code}")
